447ec060e9a96ef086d7f633af88cfc999682214
kraptastic_k <Kyle Woodburn>
2024-09-19 12:06:49 -0700

    copied files from lesson04 to begin work

---
+++ 05-Vue_Expanded/assignment/fishcamp/src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div id="notify" class="p-2 text-center">NotFound Error</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+#notify {
+  border: 3px solid orange;
+}
+</style>
